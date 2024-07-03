95c6e118ae2e8c80acfe348f2c5853b7f75cd4c5
Gabriel Moreira Gomes <gamoreir@c1r10s3.42porto.com>
2024-07-03 14:45:43 +0100

    first commit >)

---
+++ cub3d/cub3d.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmagalha <pmagalha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 10:15:19 by pmagalha          #+#    #+#             */
+/*   Updated: 2024/07/03 12:02:56 by pmagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# define SYNTAX 
+
+/*------------- Libraries ---------------*/
+
+# include "libs/libft/libft.h"
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <fcntl.h>
+
+#endif
