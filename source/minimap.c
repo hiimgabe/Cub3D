7b04fcb7119d45d7361c8d7d80630d4ea4c3ae02
hiimgabe <gabrielgmg2009@gmail.com>
2024-11-05 14:34:48 +0000

    stupidly long vertical minimap fix

--- source/minimap.c
+++ source/minimap.c
@@ -6,7 +6,7 @@
 /*   By: gabe <gabe@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:42:03 by gabe              #+#    #+#             */
-/*   Updated: 2024/10/30 18:11:36 by gabe             ###   ########.fr       */
+/*   Updated: 2024/11/05 14:33:28 by gabe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ void	draw_minimap(void)
 			else if (game()->map->layout[y][x] == '0')
 				draw_square(convert_to_screen((t_pos){x, y}), 10, MINIMAP_F);
 		}
+		if (y == 76)
+			break ;
 	}
 	draw_player();
 	draw_fov();
